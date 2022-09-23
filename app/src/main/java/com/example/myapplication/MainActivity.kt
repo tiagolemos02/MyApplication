4e8b483a16c4e57c961e3aba7f79fc9de3d464b7
tiagolemos <tiagolemos@ipvc.pt>
2022-09-23 09:41:21 +0100

    linha 11 de teste em comentário

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,4 +8,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    //teste de push para atualização de projeto em git
 }
